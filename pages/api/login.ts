abb621a215e53707840d30baadb0e342a601550d
d1av <davi4alves@gmail.com>
2022-11-03 16:33:40 -0300

    feat: authentication implemented

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import { connectMongoDB } from '../../middleware/connectDB'
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraMsg'
-import bcrypt from 'bcrypt';
 import { UsuarioModel } from "../../models/UsuarioModel";
+import { LoginResposta } from "../../types/LoginResposta";
 
 const endpointLogin = async (
   req: NextApiRequest,
-  res: NextApiResponse<RespostaPadraoMsg>
+  res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
+
+  const { JWT_KEY } = process.env;
+  if (!JWT_KEY) {
+    res.status(500).json({ error: 'ENV JWT não informado.' })
+  }
+
+
   if (req.method === 'POST') {
     const { login, senha } = req.body;
 
@@ -15,7 +25,15 @@ const endpointLogin = async (
     const buscarUsuario = await UsuarioModel.find({ email: login });
 
     if (await bcrypt.compare(senha, buscarUsuario[0].senha)) {
-      return res.status(200).json({ msg: `Usuário ${buscarUsuario[0].nome} logado com sucesso.` })
+      const usuarioLogado = buscarUsuario[0];
+
+      const token = jwt.sign({ _id: usuarioLogado._id }, JWT_KEY!);
+
+      return res.status(200).json({
+        name: usuarioLogado.nome,
+        email: usuarioLogado.email,
+        token
+      })
     }
     return res.status(405).json({ error: 'Usuário ou senha não encontrado.' })
   }
---
+++ types/LoginResposta.ts
@@ -0,0 +1,5 @@
+export type LoginResposta ={
+    name: string,
+    email:string,
+    token:string
+}
