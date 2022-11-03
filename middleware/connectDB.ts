d029852dda7de69923d8317d634a7ae1e52ac4f2
d1av <davi4alves@gmail.com>
2022-11-03 14:46:22 -0300

    feat: middleware db connect

---
+++ middleware/connectDB.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import mongoose from 'mongoose'
+
+export const connectMongoDB = (handler: NextApiHandler) =>
+    async (req: NextApiRequest, res: NextApiResponse) => {
+        // Check if database is connected
+        if (mongoose.connections[0].readyState) {
+            return handler(req, res);
+        }
+
+        // not connected then start connection
+        const { MONGO_DB_URL } = process.env;
+
+        // if env is empty, abort the system
+        if (!MONGO_DB_URL) {
+            return res.status(500).json({ error: 'ENV de configuração do banco não informada.' })
+        }
+
+        mongoose.connection.on('error', error => console.log(`Ocorreu um erro ao conectar no banco: ${error} `))
+        mongoose.connection.on('Connected', () => console.log('Banco de dados conectado'));
+        await mongoose.connect(MONGO_DB_URL);
+
+        return handler(req, res);
+    }
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { connectMongoDB } from '../../middleware/connectDB'
 
-export default function login(
+const endpointLogin = (
   req: NextApiRequest,
   res: NextApiResponse
-) {
-  if(req.method === 'POST'){
-    const {login,senha} = req.body;
+) => {
+  if (req.method === 'POST') {
+    const { login, senha } = req.body;
 
-    if((login==='admin@admin.com')&&(senha==='admin123')){
-      res.status(200).json({msg: "Usuário logado com sucesso."})
+    if ((login === 'admin@admin.com') && (senha === 'admin123')) {
+      res.status(200).json({ msg: "Usuário logado com sucesso." })
     }
-    return res.status(405).json({erro: 'Usuário ou senha não encontrado.'})
+    return res.status(405).json({ erro: 'Usuário ou senha não encontrado.' })
   }
-  return res.status(405).json({erro: 'Metodo informado não é valido'});
+  return res.status(405).json({ erro: 'Metodo informado não é valido' });
 }
+
+export default connectMongoDB(endpointLogin);
