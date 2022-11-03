58117e25460da4ed863dab7345b6561dfc69ca78
d1av <davi4alves@gmail.com>
2022-11-03 15:03:19 -0300

    feat: database connection and middleware done

--- middleware/connectDB.ts
+++ middleware/connectDB.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 import mongoose from 'mongoose'
+import type { RespostaPadraoMsg } from '../types/RespostaPadraMsg';
 
 export const connectMongoDB = (handler: NextApiHandler) =>
-    async (req: NextApiRequest, res: NextApiResponse) => {
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
         // Check if database is connected
         if (mongoose.connections[0].readyState) {
             return handler(req, res);
@@ -15,10 +16,10 @@ export const connectMongoDB = (handler: NextApiHandler) =>
         if (!MONGO_DB_URL) {
             return res.status(500).json({ error: 'ENV de configuração do banco não informada.' })
         }
-
         mongoose.connection.on('error', error => console.log(`Ocorreu um erro ao conectar no banco: ${error} `))
-        mongoose.connection.on('Connected', () => console.log('Banco de dados conectado'));
+        mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
         await mongoose.connect(MONGO_DB_URL);
+
 
         return handler(req, res);
     }
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectMongoDB } from '../../middleware/connectDB'
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraMsg'
 
 const endpointLogin = (
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<RespostaPadraoMsg>
 ) => {
   if (req.method === 'POST') {
     const { login, senha } = req.body;
 
     if ((login === 'admin@admin.com') && (senha === 'admin123')) {
-      res.status(200).json({ msg: "Usuário logado com sucesso." })
+      return res.status(200).json({ msg: "Usuário logado com sucesso." })
     }
-    return res.status(405).json({ erro: 'Usuário ou senha não encontrado.' })
+    return res.status(405).json({ error: 'Usuário ou senha não encontrado.' })
   }
-  return res.status(405).json({ erro: 'Metodo informado não é valido' });
+  return res.status(405).json({ error: 'Metodo informado não é valido' });
 }
 
 export default connectMongoDB(endpointLogin);
