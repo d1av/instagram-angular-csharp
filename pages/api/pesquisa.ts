c9d7a30032250887daf6747b0a5f2c4f91833686
d1av <davi4alves@gmail.com>
2022-11-03 23:12:23 -0300

    feat: search function added

---
+++ pages/api/pesquisa.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectMongoDB } from '../../middleware/connectDB';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraMsg'
+import { validarTokenJWT } from '../../middleware/validateToken';
+import { uploadImagemCosmic, upload } from '../../services/uploadImagemCosmic';
+import { PublicacaoModel } from '../../models/PublicacaoModel'
+import { UsuarioModel } from '../../models/UsuarioModel'
+
+const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg|Array<String>>) => {
+    try {
+        if (req.method === 'GET') {
+            const { filtro } = req.query;
+
+            if (!filtro || filtro.length < 2) {
+                return res.status(400).json({ error: 'Favor informar pelo menos mais que dois caracteres.' })
+            }
+
+            const usuariosEncontrados = await UsuarioModel.find({
+                nome: {$regex: filtro, $options: 'i'}
+            })
+            return res.status(200).json(usuariosEncontrados)
+
+        }
+        return res.status(405).json({ error: 'Metodo informado não é válido' })
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({ msg: 'A pesquisa não pode ser concluída.' })
+    }
+
+
+}
+
+export default validarTokenJWT(connectMongoDB(pesquisaEndpoint));
