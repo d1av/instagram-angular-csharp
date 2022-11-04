897be6831081e5c1ad1d17c4b0f8de2e22b1d947
d1av <davi4alves@gmail.com>
2022-11-03 23:27:19 -0300

    feat:search by id

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -9,20 +9,29 @@ import { UsuarioModel } from '../../models/UsuarioModel'
 const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | Array<String>>) => {
     try {
         if (req.method === 'GET') {
-            const { filtro } = req.query;
+            if (req?.query?.id) {
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)
+                if (!usuarioEncontrado){
+                    return res.status(400).json({error: 'Usuario não encontrado.'})
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(400).json(usuarioEncontrado)
+            } else {
+                const { filtro } = req.query;
 
-            if (!filtro || filtro.length < 2) {
-                return res.status(400).json({ error: 'Favor informar pelo menos mais que dois caracteres.' })
+                if (!filtro || filtro.length < 2) {
+                    return res.status(400).json({ error: 'Favor informar pelo menos mais que dois caracteres.' })
+                }
+
+                const usuariosEncontrados = await UsuarioModel.find({
+                    //ve por nome ou email
+                    $or: [
+                        { nome: { $regex: filtro, $options: 'i' } },
+                        { email: { $regex: filtro, $options: 'i' } },
+                    ]
+                })
+                return res.status(200).json(usuariosEncontrados)
             }
-
-            const usuariosEncontrados = await UsuarioModel.find({
-             //ve por nome ou email
-                $or: [
-                    { nome: { $regex: filtro, $options: 'i' } },
-                    { email: { $regex: filtro, $options: 'i' } },
-                ]
-            })
-            return res.status(200).json(usuariosEncontrados)
 
         }
         return res.status(405).json({ error: 'Metodo informado não é válido' })
@@ -30,8 +39,6 @@ const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<Respos
         console.log(error);
         return res.status(200).json({ msg: 'A pesquisa não pode ser concluída.' })
     }
-
-
 }
 
 export default validarTokenJWT(connectMongoDB(pesquisaEndpoint));
