15f25482aeff26429bb3be88b88b1496131058c0
d1av <davi4alves@gmail.com>
2022-11-04 21:24:17 -0300

    feat: comment section implemented

---
+++ pages/api/comments.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { connectMongoDB } from '../../middleware/connectDB';
+import { validarTokenJWT } from '../../middleware/validateToken';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraMsg';
+
+const commentEndpoint = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        if (req.method === 'PUT') {
+            const { id, userId } = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if (!usuarioLogado) {
+                return res.status(400).json({ error: 'Usuario não encontrado' })
+            }
+
+            const publicacao = await PublicacaoModel.findById(id)
+            if (!publicacao) {
+                return res.status(400).json({ error: 'Publicação não encontrada' })
+            }
+
+            if (!req.body || !req.body.comentario || req.body.comentario.length < 2) {
+                return res.status(400).json({ error: 'Comentário não é valido' })
+            }
+
+            const comentario = {
+                usuarioId: usuarioLogado._id,
+                nome: usuarioLogado.nome,
+                comentario: req.body.comentario
+            }
+            publicacao.comentarios.push(comentario);
+            await PublicacaoModel.findByIdAndUpdate({ _id: publicacao._id }, publicacao)
+            return res.status(200).json({ msg: `Comentário adiciona com sucesso por ${usuarioLogado.nome}`})
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: "Ocorreu um erro ao adicionar um comentário." })
+    }
+
+
+}
+
+export default validarTokenJWT(connectMongoDB(commentEndpoint));
