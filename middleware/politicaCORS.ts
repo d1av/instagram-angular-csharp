d1f377f9fc1f709a9ad17624287b46f472b13e2b
d1av <davi4alves@gmail.com>
2022-11-05 23:48:09 -0300

    fix: new cors middleware

--- middleware/politicaCORS.ts
+++ middleware/politicaCORS.ts
@@ -1,34 +1,18 @@
-import Cors from 'cors'
-import { NextApiRequest,NextApiResponse, NextApiHandler } from 'next'
-const express = require('express');
-const cors1 = require('cors');
-const app = express();
-app.use(cors1());
-
-// Initializing the cors middleware
-const cors = Cors({
-    methods: ['GET','POST','PUT','DELETE', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn:any) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result:any) => {
-            if (result instanceof Error) {
-                return reject(result)
-            }
-
-            return resolve(result)
-        })
-    })
-}
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import NextCors from 'nextjs-cors';
 
 export const politicaCORS = (handler: NextApiHandler) =>
     async (req: NextApiRequest, res: NextApiResponse) => {
-    // Run the middleware
-    await runMiddleware(req, res, cors)
+        try {
+            await NextCors(req, res, {
+                origin: '*',
+                methods: ['GET', 'POST', 'PUT','DELETE'],
+                optionsSuccessStatus: 200, // navegadores antigos dao problema quando se retorna 204
+            });
 
-    // Rest of the API logic
-    return handler(req,res);
-}
+            return handler(req, res);
+        } catch (e) {
+            console.log('Erro ao tratar a politica de CORS:', e);
+            return res.status(500).json({ erro: 'Ocorreu erro ao tratar a politica de CORS' });
+        }
+    }
